0c91b6ecd6fd829f4028b18eadfbabad9bd9d10b
ericbrambilaifc <ericbrambilaifc@gmail.com>
2025-10-10 11:02:59 -0300

    add explorar e add global css

---
+++ explorar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="global.css">
+    <title>Explore e encontre as melhores avaliações</title>
+</head>
+<body>
+    <header>    
+        <ul>
+            <li><a href="explorar.php">Explorar</a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </header>
+</body>
+</html>
